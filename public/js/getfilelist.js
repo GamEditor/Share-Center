6acd8dae96c5fe18b66299455eb3d5ac2c8f01ef
Mohammad <scientist.us@gmail.com>
2020-02-28 23:23:41 +0330

    directory displayer is ready

--- public/js/getfilelist.js
+++ public/js/getfilelist.js
@@ -8,7 +8,6 @@ function getListOfFiles(filesContainerId, loadingGifId, directoryDisplayerId)
 {
     const filesholder = document.getElementById(filesContainerId);
     const loadinggif = document.getElementById(loadingGifId);
-    const directoryDisplayer = document.getElementById(directoryDisplayerId);
 
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function()
@@ -66,7 +65,7 @@ function getListOfFiles(filesContainerId, loadingGifId, directoryDisplayerId)
                 {
                     file.addEventListener('dblclick', function(ev)
                     {
-                        download(file.getAttribute('path'));
+                        openFile(file.getAttribute('path'));
                     });
                 }
                 else
@@ -79,7 +78,7 @@ function getListOfFiles(filesContainerId, loadingGifId, directoryDisplayerId)
             }
 
             // showing directory on ui
-            directoryDisplayer.innerHTML = getDirectoryElements(response[0].path);
+            makeDirectoryElements(response[0].path, filesContainerId, loadingGifId, directoryDisplayerId);
         }
     };
     
@@ -89,7 +88,7 @@ function getListOfFiles(filesContainerId, loadingGifId, directoryDisplayerId)
     xhr.send();
 }
 
-function download(path)
+function openFile(path)
 {
     window.open(path);
 }
@@ -98,7 +97,6 @@ function openFolder(path, filesContainerId, loadingGifId, directoryDisplayerId)
 {
     const filesholder = document.getElementById(filesContainerId);
     const loadinggif = document.getElementById(loadingGifId);
-    const directoryDisplayer = document.getElementById(directoryDisplayerId);
 
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function()
@@ -158,7 +156,7 @@ function openFolder(path, filesContainerId, loadingGifId, directoryDisplayerId)
                     {
                         file.addEventListener('dblclick', function(ev)
                         {
-                            download(file.getAttribute('path'));
+                            openFile(file.getAttribute('path'));
                         });
                     }
                     else
@@ -176,7 +174,7 @@ function openFolder(path, filesContainerId, loadingGifId, directoryDisplayerId)
             }
 
             // showing directory on ui
-            directoryDisplayer.innerHTML = getDirectoryElements(response[0].path);
+            makeDirectoryElements(response[0].path, filesContainerId, loadingGifId, directoryDisplayerId);
         }
     };
 
@@ -186,13 +184,39 @@ function openFolder(path, filesContainerId, loadingGifId, directoryDisplayerId)
     xhr.send();
 }
 
-function getDirectoryElements(path)
+function makeDirectoryElements(path, filesContainerId, loadingGifId, directoryDisplayerId)
 {
+    var directoryDisplayer = document.getElementById(directoryDisplayerId);
+
     var folders = path.split('/');
     var result = '';
 
-    for(var i = 0; i < folders.length; i++)
-        result += '<span class="directory">' + folders[i] + '</span>/';
-    
-    return result.substring(0, result.length - 1);    // removing last / from result
+    for(let i = 0; i < folders.length; i++)
+    {
+        result += '<span id="dir-' + i + '" class="directory">' + folders[i] + '</span>/';
+    }
+
+    directoryDisplayer.innerHTML = result.substring(0, result.length - 1);
+
+    // when click on directory name, that previous folder will be open
+    // if last directory (current opened folder) so hasn't onclick listener
+    for(let i = 0; i < folders.length - 1; i++)
+    {
+        let dir = document.getElementById('dir-' + i);
+        let pathAttribute = document.createAttribute('path');
+        
+        let path = '';
+        for(let j = 0; j <= i; j++)
+            path += folders[j] + '/';
+
+        pathAttribute.value = encodeURI(path.substring(0, path.length - 1));
+        
+        dir.setAttributeNode(pathAttribute);
+        
+        dir.addEventListener('click', function(ev)
+        {
+            console.log(this.id);
+            openFolder(dir.getAttribute('path'), filesContainerId, loadingGifId, directoryDisplayerId);
+        });
+    }
 }
