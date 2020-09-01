e8848e1fe55ccdec7056c4479e6fbc0a46adae78
Mohammad <scientist.us@gmail.com>
2020-09-01 23:06:04 +0430

    temporary movie player added

--- project/public/js/mediaPlayer.js
+++ project/public/js/mediaPlayer.js
@@ -1,36 +1,65 @@
-$(window).on("load", function (event) {
-    $("#closeMediaPlayer").on("click", function (event) {
-        $("#mediaPlayer").html("<audio id='audioPlayer' controls></audio>");
-        $("#mediaPlayerContainer").hide();
+$(function (event) {
+    // music player
+    $("#closeMusicPlayer").on("click", function (event) {
+        $("#musicPlayer").html("<audio id='audioPlayer' controls></audio>");
+        $("#musicPlayerContainer").hide();
     });
 
-    $("#minimizeMediaPlayer").on("click", function (event) {
-        $("#mediaPlayerContainer").animate({
+    $("#minimizeMusicPlayer").on("click", function (event) {
+        $("#musicPlayerContainer").animate({
             left: "+=100%"
         }, 500, function () {
-            $("#maximizeMediaPlayer").show();
-            $("#maximizeMediaPlayer").css({ "width": $("#maximizeMediaPlayer").height() + "px" });
+            $("#maximizeMusicPlayer").show();
+            $("#maximizeMusicPlayer").css({ "width": $("#maximizeMusicPlayer").height() + "px" });
         });
     });
 
-    $("#maximizeMediaPlayer").on("click", function (event) {
+    $("#maximizeMusicPlayer").on("click", function (event) {
 
         $(this).hide();
-        $("#mediaPlayerContainer").animate({
+        $("#musicPlayerContainer").animate({
             left: "-=100%"
         }, 500, function () {
             // Animation complete.
         });
     });
+
+    // movie player
+    $("#closeMoviePlayer").on("click", function (event) {
+        $("#moviePlayer").html("<video id='videoPlayer' controls controlsList='nodownload'></video>");
+        $("#moviePlayerContainer").hide();
+    });
+
+    $("#minimizeMoviePlayer").on("click", function (event) {
+        var moviePlayerContainer = $("#moviePlayerContainer");
+        
+        if(moviePlayerContainer.attr("data-windowState") == "max") {
+            moviePlayerContainer.animate({
+                width: "-=60%",
+                height: "-=60%",
+            }, 500, function () {
+                $("#minimizeMoviePlayer").html("↨");
+                moviePlayerContainer.attr("data-windowState", "min");
+            });
+        } else {
+            moviePlayerContainer.animate({
+                width: "+=60%",
+                height: "+=60%",
+            }, 500, function () {
+                $("#minimizeMoviePlayer").html("-");
+                moviePlayerContainer.attr("data-windowState", "max");
+            });
+        }
+    });
 });
 
 function playAudio(path, type) {
-    let mediaPlayerContainer = $("#mediaPlayerContainer");
+    let musicPlayerContainer = $("#musicPlayerContainer");
 
-    let mediaPlayer = $("#mediaPlayer");
-    mediaPlayer.html("<audio id='audioPlayer' controls controlsList='nodownload'></audio>");
+    let musicPlayer = $("#musicPlayer");
+    musicPlayer.html("<audio id='audioPlayer' controls controlsList='nodownload'></audio>");
 
-    mediaPlayerContainer.show();
+    musicPlayerContainer.show();
 
     let audioplayer = $("#audioPlayer");
     audioplayer.attr("src", path);
